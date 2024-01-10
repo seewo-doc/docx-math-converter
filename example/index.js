fe06f1b6e4e3af5d2f5c111b60af0c5b95cc2ff8
kailunyao <yaokailun@cvte.com>
2024-01-10 08:58:48 +0800

    modify: Improve example

--- example/index.js
+++ example/index.js
@@ -1,9 +1,37 @@
+const fs = require('fs');
+const path = require('path');
 const { mathJaxReady, convertLatex2Math } = require('@seewo-doc/docx-math-converter');
+const { Document, Paragraph, Packer } = require('docx');
 
 async function test() {
   await mathJaxReady();
+  global.MathJax.config.startup.document = '';
   const result = convertLatex2Math('1+1=2');
   console.log('result', result);
+
+  const doc = new Document({
+    sections: [
+      {
+        properties: {},
+        children: [
+          new Paragraph({
+            children: [
+              result,
+            ],
+          }),
+        ],
+      },
+    ],
+  });
+
+  const buffer = await Packer.toBuffer(doc);
+  const dirPath = path.resolve(process.cwd(), '.temp');
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+
+  fs.writeFileSync(path.resolve(dirPath, 'example.docx'), buffer);
 }
 
 test();
